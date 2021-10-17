a503e00d3a04218cefead2d4d72b23e51f9fb937
waltermacias <oscarmacias191330@correo.itm.edu.co>
2021-10-17 02:04:26 -0500

    Creacion de base de datos

---
+++ Bag_repair.API/Data/Entities/ProductTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bag_repair.API.Data.Entities
+{
+     public class ProductTypes
+     {
+          public int Id { get; set; }
+          
+          [Display(Name = "Tipo de Producto")]
+          [MaxLength(50, ErrorMessage = "El campo {0} no puede tenner más de {1} carácteres")]
+          [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+          public string Description { get; set; }
+     }
+}
